05c35f5ee5202396c7afc6a649943926ead62a91
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-08-11 11:08:21 -0600

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,13 +1,25 @@
-from pdf_utils import convert_pdf_to_images  # Asume que tienes esta función en un archivo llamado "convert_pdf_to_images.py"
-
 import streamlit as st
 from PIL import Image
 from io import BytesIO
 import tempfile
 import os
+from convert_pdf_to_images import convert_pdf_to_images
+import pytesseract
+from pytesseract import image_to_string
+
+def extract_text_from_img(list_dict_final_images):
+    image_list = [list(data.values())[0] for data in list_dict_final_images]
+    image_content = []
+
+    for index, image_bytes in enumerate(image_list):
+        image = Image.open(BytesIO(image_bytes))
+        raw_text = str(image_to_string(image))
+        image_content.append(raw_text)
+
+    return "\n".join(image_content)
 
 def main():
-    st.title("Visualización de Imágenes desde PDF en Streamlit")
+    st.title("Extracción de Texto desde Imágenes en Streamlit")
 
     uploaded_file = st.file_uploader("Sube un PDF", type=["pdf"])
     if uploaded_file is not None:
@@ -16,16 +28,15 @@ def main():
         temp_file.write(uploaded_file.read())
 
         images = convert_pdf_to_images(temp_file.name)
-
-        for i, image_data in enumerate(images):
-            image_byte_array = image_data[i]
-            image = Image.open(BytesIO(image_byte_array))
-            st.image(image, caption=f"Página {i+1}", use_column_width=True)
+        text = extract_text_from_img(images)
+        
+        st.subheader("Texto Extraído de las Imágenes:")
+        st.text(text)
 
         # Cerrar y eliminar el archivo temporal
         temp_file.close()
         os.unlink(temp_file.name)
 
 if __name__ == "__main__":
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"  # Ruta al ejecutable de Tesseract en tu sistema
     main()
-
